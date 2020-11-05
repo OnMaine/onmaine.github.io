dae3bf9f095c2307d3e76023823012e8818a51b9
Володимир <onmaine@gmail.com>
2020-11-05 14:57:11 +0200

    add close-btn

--- task2/js/index.js
+++ task2/js/index.js
@@ -1,51 +1,3 @@
-// const users = [
-//   {
-//     name: 'Смородина Алена Александровна',
-//     phone: '0987658923',
-//     mail: 'alenasmorodina@gmail.com',
-//     skype: 'alenasmorodina',
-//     comment: 'Встреча завтра на 11:00.',
-//     info: {
-//       data: '11.12.2012',
-//       age: '25',
-//       maritalStatus: 'замужем',
-//       job: 'Епам, менеджер',
-//       education: 'Высшее, НАУ , Компютерные науки'
-//     }
-//   },
-//   {
-//     name: 'Мироненнко-Коновалов Александр Игнатьевич',
-//     phone: '0506123123',
-//     mail: 'milanovalex@ua.fm',
-//     skype: 'Milanovalex',
-//     comment: 'Созвониться в пятницу 05.07.2019.',
-//     info: {
-//       data: '21.05.2018',
-//       age: '23',
-//       maritalStatus: 'холост',
-//       job: 'Фора, кассир',
-//       education: 'Высшее, НТУ КПИ, Финансы и кредит'
-//     }
-//   },
-//   {
-//     name: 'Зайченко Валентина Александровна',
-//     phone: '0966671290',
-//     mail: 'zay4enkovalya@mail.ru',
-//     skype: 'zay4enkovalya',
-//     comment: 'Очень занят в ближайшие 2 недели. 10.05.2019.',
-//     info: {
-//       data: '15.02.2015',
-//       age: '27',
-//       maritalStatus: 'замужем',
-//       job: 'Кристал, фитнес-тренер',
-//       education: 'Высшее, КНТУ, Физиотерапия и реабилитация'
-//     }
-//   },
-// ];
-//
-// let json = JSON.stringify(users);
-
-
 let users = `[{
   "phone": "0987658923",
   "info": {
@@ -75,14 +27,9 @@ let users = `[{
   }
 }]`;
 
-
-
 users = $.parseJSON(users);
 
-
-
 $(document).on('click', '#userdata tbody td', function(event) {
-
   let $col = $(this);
   let $row = $col.parents('tr').addClass('selected').siblings().removeClass('selected');
   let $id = $('.selected td:eq(1)');
@@ -97,10 +44,11 @@ $(document).on('click', '#userdata tbody td', function(event) {
                 Работает: ${details.job}
                 Образование: ${details.education}
                 `;
-        $('.details-cell').text(detailsString);
+        $('.details-box').text(detailsString);
         $('thead tr th:not(:first), tbody tr td:not(tr td:first-of-type)').hide();
-        $('thead tr th:first, tbody tr td:first').css({'width': '500px'});
-
+        $('thead tr th:first, tbody tr td:first').css({
+          'width': '500px'
+        });
         $('.details-hCell, .details-cell').show();
       }
     });
@@ -109,53 +57,6 @@ $(document).on('click', '#userdata tbody td', function(event) {
 
 
 
-
-// $(document).ready(function() {
-//   $("#userdata tbody tr").on("click", function() {
-//     let myIndex = $("#userdata tbody tr").index(this);
-//     $(this).parents("table").find("tr").each(function() {
-//       $.each(users, function(i, obj) {
-//
-//       });
-//       $(this).find("th:eq(" + myIndex + ")").remove();
-//       $(this).find("td:eq(" + myIndex + ")").remove();
-//       $('tr td:last-of-type').find("td:eq(" + myIndex + ")").alert("");
-//     });
-//   });
-// });
-
-// $('tr th:last-of-type').show();
-// $(this).find("th:eq(" + myIndex + ")").show();
-// $('tr td:last-of-type').find("td:eq(" + myIndex + ")").show();
-// $('tr td:last-of-type').show();
-
-
-
-
-
-
-
-
-
-
-//
-// $(document).ready(function(){
-//    $("#userdata").click(function(event){
-//      $('tr td:last-of-type, tr th:last-of-type').show();
-//      $('tr td:last-of-type, tr th:last-of-type').hide();
-//    });
-//  });
-
-
- //
- // $(document).ready(function() {
- //   $("#userdata tbody tr").on("click", function() {
- //     var myIndex = $("#userdata tbody tr").index(this);
- //     $(this).parents("table").find("tr").each(function() {
- //       $('tr th:last-of-type').show();
- //       // $(this).find("th:eq(" + myIndex + ")").show();
- //       // $('tr td:last-of-type').find("td:eq(" + myIndex + ")").show();
- //       $('tr td:last-of-type').show();
- //     });
- //   });
- // });
+$(document).on('click', '.btn-close', function(event) {
+  location.reload();
+});
