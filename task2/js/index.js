f4e6074e24f547a46e93cf72b79229f09dc180d9
Володимир <onmaine@gmail.com>
2020-11-05 04:03:56 +0200

    new table

--- task2/js/index.js
+++ task2/js/index.js
@@ -45,107 +45,74 @@
 //
 // let json = JSON.stringify(users);
 
+
 let users = `[{
+  "phone": "0987658923",
+  "info": {
     "data": "11.12.2012",
     "age": "25",
     "maritalStatus": "замужем",
     "job": "Епам, менеджер",
     "education": "Высшее, НАУ , Компютерные науки"
+  }
 }, {
+  "phone": "0506123123",
+  "info": {
     "data": "21.05.2018",
     "age": "23",
     "maritalStatus": "холост",
     "job": "Фора, кассир",
     "education": "Высшее, НТУ КПИ, Финансы и кредит"
+  }
 }, {
+  "phone": "0966671290",
+  "info": {
     "data": "15.02.2015",
     "age": "27",
     "maritalStatus": "замужем",
     "job": "Кристал, фитнес-тренер",
     "education": "Высшее, КНТУ, Физиотерапия и реабилитация"
+  }
 }]`;
+
 
 
 users = $.parseJSON(users);
 
-// function createTable(users) {
-//   let tBody = $("#userdata tbody tr").tBodies[0];
 //
-//   let myIndex = $("#userdata tbody tr").index(this);
-//   $(this).parents("table").find("tr").each(function() {
-//     $.each(users, function(i, obj) {
-//
-//     });
-//   });
-// };
-
-// createTable(users);
-
 //
 // $(document).ready(function() {
 //   $("#userdata tbody tr").on("click", function() {
-//     let tBody = $("#userdata tbody tr").tBodies[0];
 //     let myIndex = $("#userdata tbody tr").index(this);
 //     $(this).parents("table").find("tr").each(function() {
-//       // $('tr th:last-of-type').show();
-//       // $(this).find("th:eq(" + myIndex + ")").show();
-//       // $('tr td:last-of-type').find("td:eq(" + myIndex + ")").show();
-//       // $('tr td:last-of-type').show();
-//       $(this).find("th:eq(" + myIndex + ")").remove();
-//       $(this).find("td:eq(" + myIndex + ")").remove();
+//
+//         $(this).find("th:eq(" + myIndex + ")").show();
 //     });
 //   });
 // });
 
-
-
-
 //
-// $(document).ready(function(){
-//    $("#userdata").click(function(event){
-//      $('tr td:last-of-type, tr th:last-of-type').show();
-//      $('tr td:last-of-type, tr th:last-of-type').hide();
-//    });
-//  });
+$(document).on('click', '#userdata tbody td', function(event) {
 
-
- //
- // $(document).ready(function() {
- //   $("#userdata tbody tr").on("click", function() {
- //     var myIndex = $("#userdata tbody tr").index(this);
- //     $(this).parents("table").find("tr").each(function() {
- //       $('tr th:last-of-type').show();
- //       // $(this).find("th:eq(" + myIndex + ")").show();
- //       // $('tr td:last-of-type').find("td:eq(" + myIndex + ")").show();
- //       $('tr td:last-of-type').show();
- //     });
- //   });
- // });
-
-
-
-$(document).ready(function() {
-  $("#userdata").on("click", function() {
-    /* Better index-calculation from @activa */
-    var myIndex = $('th #userdata').index(this);
-    $(this).parents("table").find("tr").each(function() {
-      $(this).find("th:eq(" + myIndex + ")").remove();
-      $(this).find("td:eq(" + myIndex + ")").remove();
+  let $col = $(this);
+  let $row = $col.parents('tr').addClass('selected').siblings().removeClass('selected');
+  let $id = $('.selected td:eq(1)');
+  let phone = $id.text();
+  $.each(users, function(index, users) {
+    $.each(users, function(key, val) {
+      if (key == "phone" && val == phone) {
+        let details = users.info;
+        const detailsString = `
+                Добавлен: ${details.data}
+                Возраст: ${details.age}
+                Семейное положение: ${details.maritalStatus}
+                Работает: ${details.job}
+                Образование: ${details.education}
+                `;
+        console.log(detailsString);
+        documentQuerySelector(‘.details-cell’).text(detailsString)
+        $('.my #awesome selector');
+      }
     });
   });
 });
-
-
-
-
- $(document).ready(function() {
-   $("#userdata tbody tr").on("click", function() {
-     var myIndex = $("#userdata tbody tr").index(this);
-     $(this).parents("table").find("tr").each(function() {
-       $('tr th:last-of-type').show();
-       // $(this).find("th:eq(" + myIndex + ")").show();
-       $('tr td:last-of-type').find("td:eq(" + myIndex + ")").show();
-       $('tr td:last-of-type').show();
-     });
-   });
- });
